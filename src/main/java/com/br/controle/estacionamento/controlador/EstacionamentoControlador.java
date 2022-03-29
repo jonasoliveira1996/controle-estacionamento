a22b2ff970ddda12419ed3a305d081c80ca4b81a
Jonas Menotti <jonasoliveira1996@gmail.com>
2022-03-29 14:27:43 -0300

    Front-end adicionado

--- src/main/java/com/br/controle/estacionamento/controlador/EstacionamentoControlador.java
+++ src/main/java/com/br/controle/estacionamento/controlador/EstacionamentoControlador.java
@@ -1,6 +1,7 @@
 package com.br.controle.estacionamento.controlador;
 
 import java.time.LocalDateTime;
+
 import java.time.ZoneId;
 import java.util.List;
 import java.util.Optional;
@@ -10,11 +11,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import com.br.controle.estacionamento.modelo.Estacionamento;
 import com.br.controle.estacionamento.repositorios.EstacionamentoRepositorio;
@@ -31,8 +33,9 @@ public class EstacionamentoControlador {
 			return ResponseEntity.status(HttpStatus.OK).body(estacionamentoRepositorio.findAll());
 		}
 		
-		@GetMapping("/{id}")
-		public ResponseEntity<Object> obterRegistrosPorId(@PathVariable Long id) {
+		@GetMapping("/buscarPorId")
+		@ResponseBody
+		public ResponseEntity<Object> obterRegistrosPorId(@RequestParam(name="id") Long id) {
 			
 			Optional<Estacionamento> estacionamentoOptional = estacionamentoRepositorio.findById(id);
 			if(!estacionamentoOptional.isPresent()) {
@@ -42,41 +45,56 @@ public class EstacionamentoControlador {
 			return ResponseEntity.status(HttpStatus.OK).body(estacionamentoOptional.get());
 		}
 		
-		@PostMapping
+		@GetMapping(value="/buscarPorNome")
+		@ResponseBody
+		public ResponseEntity<List<Estacionamento>> buscarPorNome(@RequestParam(name = "nomeResponsavel") String nomeResponsavel ) {
+			
+			List<Estacionamento> estacionamento = estacionamentoRepositorio.findByNomeResponsavel(nomeResponsavel.trim().toUpperCase());
+			
+			return ResponseEntity.status(HttpStatus.OK).body(estacionamento);
+		}
+		
+		@PostMapping(value="/cadastrar")
+		@ResponseBody
 		public ResponseEntity<Object> inserirRegistro(@RequestBody @Valid Estacionamento estacionamento) {
 			
 			estacionamento.setDataRegistro(LocalDateTime.now(ZoneId.of("UTC")));
 						
 			if(estacionamentoRepositorio.existsByPlacaCarro(estacionamento.getPlacaCarro())) {
-				return ResponseEntity.status(HttpStatus.CONFLICT).body("Conflito: Placa do carro já existe.");
+				return ResponseEntity.status(HttpStatus.CONFLICT).body("Placa do carro já existe.");
 			}
 			
 			if(estacionamentoRepositorio.existsBynumeroVagaEstacionamento(estacionamento.getNumeroVagaEstacionamento())) {
-				return ResponseEntity.status(HttpStatus.CONFLICT).body("Conflito: Vaga de estacionamento já está em uso.");
+				return ResponseEntity.status(HttpStatus.CONFLICT).body("Vaga de estacionamento já está em uso.");
 			}
 			
 			if(estacionamentoRepositorio.existsByApartamentoAndBloco(estacionamento.getApartamento(), estacionamento.getBloco())) {
-				return ResponseEntity.status(HttpStatus.CONFLICT).body("Conflito: Apartamento/bloco já está em uso.");
+				return ResponseEntity.status(HttpStatus.CONFLICT).body("Apartamento/bloco já está em uso.");
 			}
 			
 			return ResponseEntity.status(HttpStatus.CREATED).body(estacionamentoRepositorio.save(estacionamento));
 		}
 		
-		@PutMapping
+		@PutMapping("/editar")
+		@ResponseBody
 		public ResponseEntity<Object> alterarRegistro(@RequestBody @Valid Estacionamento estacionamento){
 			
-			Optional<Estacionamento> estacionamentoOptional = estacionamentoRepositorio.findById(estacionamento.getId());
+			estacionamento.setDataRegistro(LocalDateTime.now(ZoneId.of("UTC")));
 			
+			Optional<Estacionamento> estacionamentoOptional = estacionamentoRepositorio.findById(estacionamento.getId());
+				
 			if(!estacionamentoOptional.isPresent()) {
 				return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Registro não encontrada");
 			}
 			
 			estacionamentoRepositorio.save(estacionamento);
 			return ResponseEntity.status(HttpStatus.OK).body("Registro alterado com sucesso.");
+	
 		}
 
-		@DeleteMapping("/{id}")
-		public ResponseEntity<Object> excluirRegistro(@PathVariable Long id){
+		@DeleteMapping("/deletarPorId")
+		@ResponseBody
+		public ResponseEntity<Object> excluirRegistro(@RequestParam(name="id") Long id){
 			
 			Optional<Estacionamento> estacionamentoOptional = estacionamentoRepositorio.findById(id);
 			
--- src/main/java/com/br/controle/estacionamento/repositorios/EstacionamentoRepositorio.java
+++ src/main/java/com/br/controle/estacionamento/repositorios/EstacionamentoRepositorio.java
@@ -1,6 +1,10 @@
 package com.br.controle.estacionamento.repositorios;
 
+ 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import com.br.controle.estacionamento.modelo.Estacionamento;
 
@@ -10,4 +14,8 @@ public interface EstacionamentoRepositorio extends JpaRepository<Estacionamento,
 		public boolean existsByPlacaCarro(String placaCarro);
 		public boolean existsBynumeroVagaEstacionamento(String numeroVagaEstacionamento);
 		public boolean existsByApartamentoAndBloco(String apartamento, String bloco);
+		public boolean existsById(Long id);
+
+		@Query(value = "select u from Estacionamento u where upper(trim(u.nomeResponsavel)) like %?1%")
+		public List<Estacionamento> findByNomeResponsavel(String nomeResponsavel);
 }
