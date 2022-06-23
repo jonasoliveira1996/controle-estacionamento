5d59f2c81734732480ad27084dec3569f4c04a34
Jonas Menotti <jonasoliveira1996@gmail.com>
2022-06-23 19:13:48 -0300

    configuracao

--- src/main/java/com/br/controle/estacionamento/config/DateConfig.java
+++ src/main/java/com/br/controle/estacionamento/config/DateConfig.java
@@ -1,7 +1,10 @@
 package com.br.controle.estacionamento.config;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.time.format.DateTimeFormatter;
 
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -25,4 +28,20 @@ public class DateConfig {
 		return new ObjectMapper().
 				registerModule(module);
 	}
+	
+	@Bean
+    public BasicDataSource dataSource() throws URISyntaxException {
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() + "?sslmode=require";
+
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setUrl(dbUrl);
+        basicDataSource.setUsername(username);
+        basicDataSource.setPassword(password);
+
+        return basicDataSource;
+    }
 }
