98e3e599da6091b35e2ea640cb506d36ae044e39
Jonas Menotti <jonasoliveira1996@gmail.com>
2022-06-25 11:25:05 -0300

    Adicionar tabela ao BD heroku

--- src/main/java/com/br/controle/estacionamento/ControleEstacionamentoApplication.java
+++ src/main/java/com/br/controle/estacionamento/ControleEstacionamentoApplication.java
@@ -2,8 +2,10 @@ package com.br.controle.estacionamento;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EntityScan(basePackages = "com.br.controle.estacionamento.modelo")
 @EnableJpaRepositories(basePackages = "com.br.controle.estacionamento.repositorios")
 @SpringBootApplication
 public class ControleEstacionamentoApplication {
